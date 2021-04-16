46a2116c61d8bae7dd2c51bdc3f44b271a35bd79
Mary Morvan <mpmorvan@talbotdrake.com>
2021-04-16 09:08:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,215 +1,84 @@
-// Identified the variables
-var uppercase = "ABDCEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase = uppercase.toLowerCase();
-var special = "!@#$%^&*?-";
-var numbers = "1234567890";
-var password = '';
- 
-var genBtn = document.querySelector("#gen");
-var textarea = document.querySelector("#textarea");
-  
-function generatePassword() {
+/*Not sure why the alert does not initially appear to begin the process. Explored diff options
+in the button functionality and tried to connect it to the javascript function.
 
- //Questions for the user in the pop-ups
- var userSpecial = confirm("Would you like your password to create special characters?");
- var userNumber = confirm("Would you like your password to include numbers?");
- var userUCase = confirm("Would you like to include uppercase letters?");
- var userLCase = confirm("Would you like to include lowercase letters?")
+Also could not get rid of the black border around the submit button when clickec....
+*/
 
- //Utilizing the var for true and false statements
- var userUCaseTrue = userUCase === true;
- var userUCaseFalse = userUCase === false;
- var userLCaseTrue = userLCase === true;
- var userLCaseFalse = userLCase === false;
- var userSpecialTrue = userSpecial === true;
- var userSpecialFalse = userSpecial === false;
- var userNumberTrue = userNumber === true;
- var userNumberFalse = userNumber === false;
 
- if (userSpecialFalse && userNumberFalse && userUCaseFalse && userLCaseFalse) {
-            alert("You must include at least one option");
-            return;
- }
- else {
-    var userLength = prompt("Enter a number from 8 - 128 to set the length of your password");
- }
+var numberArray = [
+    '0',
+    '1',
+]
+var numbers = "90123456789";
+var symbols = "!@#$%^&*()_+?/";
+var lowerLetters = "abcdefghijklmnopqrstuv";
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var pwd = '';
+var allCharacters = '';
 
- if (!isNumber(userLength)){
-        alert("Please enter a number from 8 - 128");
-        userLength = prompt("Enter a number from 8 - 128 to set the length of your password");
-    }
+var numbersChoice = false;
+var symbolsChoice = false;
+var lowerChoice = false;
+var upperChoice = false;
 
- if (userLength < 8 || userLength > 128) {
-    alert("Please enter a number between 8 - 128");
- }
- else {
+document.getElementById("newestpassword").onclick = generate;
 
-        if (userSpecialTrue && userNumberTrue && userUCaseTrue && userLCaseTrue) {
-            var charSet = special + numbers + uppercase + lowercase;
+function generate() {
+    var verifyLength = '';
 
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
 
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-
-        if (userSpecialFalse && userNumberTrue && userUCaseTrue && userLCaseTrue) {
-            var charSet = numbers + uppercase + lowercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-
-        if (userSpecialFalse && userNumberFalse && userUCaseTrue && userLCaseTrue) {
-            var charSet = uppercase + lowercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-
-        if (userSpecialFalse && userNumberFalse && userUCaseFalse && userLCaseTrue) {
-            var charSet = lowercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialTrue && userNumberFalse && userUCaseTrue && userLCaseTrue) {
-            var charSet = special + uppercase + lowercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialTrue && userNumberTrue && userUCaseFalse && userLCaseTrue) {
-            var charSet = special + numbers + lowercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialTrue && userNumberTrue && userUCaseTrue && userLCaseFalse) {
-            var charSet = special + numbers + uppercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialTrue && userNumberTrue && userUCaseFalse && userLCaseFalse) {
-            var charSet = special + numbers;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialTrue && userNumberFalse && userUCaseFalse && userLCaseTrue) {
-            var charSet = special + lowercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialTrue && userNumberTrue && userUCaseTrue && userLCaseFalse) {
-            var charSet = special + numbers + uppercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialTrue && userNumberFalse&& userUCaseFalse && userLCaseFalse) {
-            var charSet = special;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialFalse && userNumberTrue && userUCaseTrue && userLCaseFalse) {
-            var charSet = numbers + uppercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialFalse && userNumberTrue && userUCaseFalse && userLCaseTrue) {
-            var charSet = special + numbers + uppercase;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
-        }
-        if (userSpecialFalse && userNumberTrue && userUCaseFalse && userLCaseFalse) {
-            var charSet =  numbers;
-
-            for ( var i = 0; i < userLength; i++) {
-            password += charSet[Math.floor(Math.random() * charSet.length)];
-            }
-
-            textarea.textContent = password;
-            console.log("Here's your password: " + password);
-            return password;
+    while (isNaN(verifyLength) || verifyLength < 10 || verifyLength > 50) {
+        verifyLength = prompt("How many characters do you want for you password?(Must be between 10 - 50.)");
+        if (verifyLength === null) {
+            break;
         }
     }
+    if (verifyLength) {
+        if (confirm("Will you use numbers?") == true) { numbersChoice = true }
+        if (confirm("Will you use characters?") == true) (symbolsChoice = true)
+        if (confirm("Will you use lowercase alphabets?") == true) { lowerChoice = true }
+        if (confirm("Will you use uppercase alphabets>") == true) { upperChoice = true }
+    }
+
+    if (numbersChoice === false && symbolsChoice === false && lowerChoice === false && upperChoice === false) {
+        alert("One of each character type must be selected.")
+    }
+
+
+
+    allCharacters += (numbersChoice ? numbers : '');
+    allCharacters += (symbolsChoice ? symbols : '');
+    allCharacters += (lowerChoice ? lowerLetters : '');
+    allCharacters += (upperChoice ? upperLetters : '');
+
+    pwd = password(verifyLength);
+    document.getElementById('password').innerHTML = pwd;
+
 }
 
-//The event listeners for the buttons
-genBtn.addEventListener("click", generatePassword);
-document.querySelector("#copy").addEventListener("click", copy);
 
-function isNumber(value) {
-    var numberPattern = /^[0-9]+$/;
-    return numberPattern.test(value);
+
+function password(length) {
+    var psswd = '';
+    for (var i = 0; i < length; i++) {
+        psswd += allCharacters.charAt(Math.floor(Math.random() * length))
+    }
+    return psswd;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
